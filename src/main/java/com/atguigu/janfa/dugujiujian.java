39ffd2d0ae198cdfebacf6579bdeebc3ddff4d5b
liufeiyang2 <1184277345@qq.com>
2019-03-27 20:20:19 +0800

    wangzqz修改

--- src/main/java/com/atguigu/janfa/dugujiujian.java
+++ src/main/java/com/atguigu/janfa/dugujiujian.java
@@ -5,5 +5,10 @@ public class dugujiujian {
     public static void main(String[] args) {
 
         System.out.println(" 你太善良");
+        System.out.println(" 你太善良");
+        System.out.println(" 你太善良");
+        System.out.println(" 你太善良");
+        System.out.println(" 你太善良");
+
     }
 }
